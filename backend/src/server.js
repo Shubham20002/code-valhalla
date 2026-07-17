e5da88923aa00cc77c1452320fb2d013557a3ab6
shubham <shubham992284@gmail.com>
2026-07-17 23:52:22 +0530

    initial fe,be setup completed

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {ENV} from "./lib/env.js"
+
+const app=express();
+
+app.get("/health",(req,res)=>{
+    res.status(400).json({ msg:"api is up and running"})
+})
+
+app.listen(ENV.PORT,()=>console.log("server is running on port",ENV.PORT))
