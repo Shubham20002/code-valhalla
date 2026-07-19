681836cdc085dbf02392386590dc309845fc5873
shubham <shubham992284@gmail.com>
2026-07-19 18:51:39 +0530

    Add backend and frontend package configurations, update environment variables, and enhance server deployment logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
-import {ENV} from "./lib/env.js"
+import {ENV} from "./lib/env.js";
+import path from "path"
 
 const app=express();
+
+const __dirname=path.resolve()
 
 app.get("/health",(req,res)=>{
     res.status(400).json({ msg:"api is up and running"})
 })
+//make our app ready for deployment
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>res.sendFile(express.static(path.join(__dirname,"../frontend/dist/index.html"))))
+}
 
 app.listen(ENV.PORT,()=>console.log("server is running on port",ENV.PORT))
