5213dd752ed05b7ff29ff6271d1a1aadad407942
shubham <shubham992284@gmail.com>
2026-07-20 23:33:37 +0530

    fix: import functions from inngest module in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import {connectDB} from "./lib/db.js"
 import path from "path"
 import cors from "cors";
 import { serve } from "inngest/express";
-import { inngest } from "./lib/inngest.js";
+import { inngest,functions  } from "./lib/inngest.js";
 
 const app=express();
 
